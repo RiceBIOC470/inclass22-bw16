f45dba3f032d1bb37e5a188b742165726be7da93
bw16 <31257600+bw16@users.noreply.github.com>
2017-11-27 10:48:59 -0600

    Add files via upload

--- Inclass22.m
+++ Inclass22.m
@@ -6,5 +6,50 @@
 %transcripton factor are necessary to activate transciption. Comment on your
 %results. 
 
+figure;
+hold on;
+ku=0;
+for kb=0:0.05:5
+    polycoeff=[1 -kb -ku];
+    rts=roots(polycoeff);
+    rts=rts(imag(rts)==0);
+    plot(kb*ones(length(rts),1),rts,'r.');
+end
+hold off;
+
+figure;
+hold on;
+ku=0;
+for kb=0:0.5:5
+    polycoeff=[1 -kb 0 0 1 -ku]; %x^5 -kb*x^4 + x - ku = 0
+    rts=roots(polycoeff);
+    rts=rts(imag(rts)==0);
+    plot(kb*ones(length(rts),1),rts,'r.');
+end
+hold off;
+
+
+figure; 
+hold on;
+ku=0;
+for kb=0:0.5:5
+    polycoeff=[1 -kb 0 0 0 0 0 0 1 -ku]; %x^9 -kb*x^8 + x - ku = 0
+    rts=roots(polycoeff);
+    rts=rts(imag(rts)==0);
+    plot(kb*ones(length(rts),1),rts,'r.');
+end
+hold off;
+
 % 2. Make a similar diagram for the case of an autorepressing gene in the
-% case that 2 copies are need to turn off the gene. 
+% case that 2 copies are need to turn off the gene.
+figure;
+hold on;
+ku=0;
+for kb=0:0.5:5
+    polycoeff=[1 kb 1 -ku];
+    rts=roots(polycoeff);
+    rts=rts(imag(rts)==0);
+    plot(kb*ones(length(rts),1),rts,'r.');
+   
+end
+hold off;
